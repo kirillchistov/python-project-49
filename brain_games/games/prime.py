4261b55b699e83911a04a813802469db2abedddf
Kirill Chistov <kirill.v.chistov@yandex.ru>
2024-01-31 13:32:17 +0300

    Fix bugs in prime description

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -2,8 +2,7 @@
 from random import randint
 import math
 
-DESCRIPTION = '''Answer "yes" if given number is prime.
-Otherwise answer "no".'''
+DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(number):
